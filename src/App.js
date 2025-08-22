0fa544e7239518453f9902b73e6a6f4f7885baeb
Pawel Lubrycht <plubrycht@gmail.com>
2025-08-22 16:10:40 +0200

    feat/new images and styles

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import MainPage from './pages/main-page/MainPage.jsx';
 import PrivacyPolicy from './pages/privacy-policy/PrivacyPolicy.jsx';
 import ScrollToTop from './components/scroll-to-top/ScrollToTop.jsx';
 import React from 'react';
-import './App.css'; // Import global styles if needed
+import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+import { useEffect } from 'react';// Import global styles if needed
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800, // czas animacji w ms
+      once: true,    // animacja tylko raz przy przewinięciu
+    });
+  }, []);
   return (
     <Router>
       <ScrollToTop /> 
--- src/pages/main-page/components/HeroSection/HeroSection.jsx
+++ src/pages/main-page/components/HeroSection/HeroSection.jsx
@@ -14,10 +14,13 @@ const HeroSection = () => {
   return (
     <div className="hero-section" id="hero">
       <Navbar />
-          <div className="hero-content">
-          <h1>Umów się na bezpłatną diagnostykę mowy!</h1>
-          <button onClick={() => scrollToSection('booking')}>Umów się</button>
-          </div>  
+      <div className="hero-background">
+        <div className="hero-content">
+          <div className="hero-blur"></div>
+            <h1  data-aos="fade-up" >Umów się na bezpłatną diagnostykę mowy!</h1>
+            <button  onClick={() => scrollToSection('booking')}>Umów się</button>
+          </div>
+        </div>
       </div>
     
   );
